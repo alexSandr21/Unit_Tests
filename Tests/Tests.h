20fe21f876f17e030df9577a65e21088d674440a
Alexand_PC\Alexandr <alexashka574@gmail.com>
2018-04-08 14:20:06 +0300

    Added more cases for url parser tests and also added negative tests with parsing of  wrong url string

--- Tests/Tests.h
+++ Tests/Tests.h
@@ -6,6 +6,7 @@
 #include "Utils.h"
 
 const std::string URL_STR = "http://host/path/subpath?name1=value1&name2=\"value2\"&\"name3\"=#fragment";
+const std::string WRONG_URL_STR = "http:;//host@port/path?/subpath?name==value1#fragment";
 
 TEST(UtilsTest, EncodingUtilsTest)
 {
@@ -27,6 +28,35 @@ TEST(UrlTest, ParsingUrlStringTest)
     EXPECT_TRUE(url_str.Path == L"/path/subpath");
     EXPECT_TRUE(url_str.Query[0].first == L"name1");
     EXPECT_TRUE(url_str.Fragment == L"fragment");
+
+    url_str = Url("http://host/path/subpath");
+    EXPECT_TRUE(url_str.Scheme == L"http");
+    EXPECT_TRUE(url_str.Authority == L"host");
+    EXPECT_TRUE(url_str.Path == L"/path/subpath");
+    EXPECT_TRUE(url_str.Query.size() == 0);
+    EXPECT_TRUE(url_str.Fragment == L"");
+
+    url_str = Url("http://host");
+    EXPECT_TRUE(url_str.Scheme == L"http");
+    EXPECT_TRUE(url_str.Authority == L"host");
+    EXPECT_TRUE(url_str.Path == L"");
+    EXPECT_TRUE(url_str.Query.size() == 0);
+    EXPECT_TRUE(url_str.Fragment == L"");
+
+    url_str = Url("");
+    EXPECT_TRUE(url_str.Scheme == L"");
+    EXPECT_TRUE(url_str.Authority == L"");
+    EXPECT_TRUE(url_str.Path == L"");
+    EXPECT_TRUE(url_str.Query.size() == 0);
+    EXPECT_TRUE(url_str.Fragment == L"");
+
+    Url wrong_url_str(WRONG_URL_STR);
+    EXPECT_TRUE(wrong_url_str.Scheme == L"http");
+    EXPECT_FALSE(wrong_url_str.Authority == L"host@port");
+    EXPECT_FALSE(wrong_url_str.Path == L"/path?/subpath");
+    EXPECT_FALSE(wrong_url_str.Query.size() == 0);
+    EXPECT_TRUE(wrong_url_str.Fragment == L"fragment");
+
 }
 
 TEST(UrlTest, ParsingQueryTest)
@@ -47,6 +77,7 @@ TEST(UrlTest, ParsingQueryTest)
 TEST(UrlTest, CompileUrlTest)
 {
     {
+        //ASCII query
         auto args = Url::QueryArgs() ;
         args.emplace_back(L"name1", L"value1");
         args.emplace_back(L"name2", L"value2");
@@ -57,10 +88,14 @@ TEST(UrlTest, CompileUrlTest)
         EXPECT_TRUE(url_str.Compile() == "http://host/path/subpath?name1=value1&name2=value2&=&name3=value3#fragment");
     }
     {
+        //non ASCII query
         auto args = Url::QueryArgs() ;
         args.emplace_back(L"n\"aоme1", L"vaКlue1");
 
         Url url_str(L"http", L"host", L"/p at h/sub p'ath", L"fragment", args);
         EXPECT_TRUE(url_str.Compile() == "http://host/p%20at%20h/sub%20p'ath?n%22a%D0%A0%D1%95me1=va%D0%A0%D1%99lue1#fragment");
+    }
+    {
+        EXPECT_TRUE(Url().Compile() == "");
     }
 }
